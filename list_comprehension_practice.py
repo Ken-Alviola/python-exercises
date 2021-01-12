470f99cc8ceb6214b0867cd2a156c662a5dceabe
Ken-Alviola <kenalviola@gmail.com>
2021-01-12 15:18:42 -0600

    updating list comp

--- list_comprehension_practice.py
+++ list_comprehension_practice.py
@@ -23,20 +23,18 @@ for fruit in fruits:
 uppercased_fruits = [fruit.upper() for fruit in fruits]
 uppercased_fruits
 
-
 # Exercise 2 - create a variable named capitalized_fruits and use 
 # list comprehension syntax to produce output like ['Mango', 'Kiwi', 'Strawberry', etc...]
-
 capitalized_fruits = [fruit.capitalize() for fruit in fruits]
 capitalized_fruits
 
-# Exercise 3 - Use a list comprehension to make a variable named fruits_with_more_than_two_vowels. Hint: You'll need a way to check if something is a vowel.
 
+## Exercise 3 - Use a list comprehension to make a variable named fruits_with_more_than_two_vowels. Hint: You'll need a way to check if something is a vowel.
 vowels = ['a','e','i','o','u']
-fruits_with_more_than_two_vowels = [f for fruit in fruits if fruit in vowels]
+fruits_with_more_than_two_vowels = [fruit for fruit in fruits if fruit.split() in vowels]
 fruits_with_more_than_two_vowels
 
-# Exercise 4 - make a variable named fruits_with_only_two_vowels. The result should be ['mango', 'kiwi', 'strawberry']
+## Exercise 4 - make a variable named fruits_with_only_two_vowels. The result should be ['mango', 'kiwi', 'strawberry']
 
 # Exercise 5 - make a list that contains each fruit with more than 5 characters
 fruits_with_more_than_5_characters = [fruit for fruit in fruits if len(fruit) > 5]
@@ -47,25 +45,51 @@ fruits_with_5_characters = [fruit for fruit in fruits if len(fruit) == 5]
 fruits_with_5_characters
 
 # Exercise 7 - Make a list that contains fruits that have less than 5 characters
+fruits_with_less_than_5_characters = [fruit for fruit in fruits if len(fruit) < 5]
+fruits_with_less_than_5_characters
 
 # Exercise 8 - Make a list containing the number of characters in each fruit. Output would be [5, 4, 10, etc... ]
+number_of_char = [len(fruit) for fruit in fruits]
+number_of_char
 
-# Exercise 9 - Make a variable named fruits_with_letter_a that contains a list of only the fruits that contain the letter "a"
+## Exercise 9 - Make a variable named fruits_with_letter_a that contains a list of only the fruits that contain the letter "a"
+fruits_with_letter_a = [fruit for fruit in fruits if fruit.split() in 'a']
+fruits_with_letter_a
+
+splits = [fruit.split(),]
 
 # Exercise 10 - Make a variable named even_numbers that holds only the even numbers 
+even_numbers = [number for number in numbers if number % 2 == 0]
+even_numbers
 
 # Exercise 11 - Make a variable named odd_numbers that holds only the odd numbers
+odd_numbers = [number for number in numbers if number % 2 != 0]
+odd_numbers
 
 # Exercise 12 - Make a variable named positive_numbers that holds only the positive numbers
+positive_numbers = [number for number in numbers if number > 0]
+positive_numbers
 
 # Exercise 13 - Make a variable named negative_numbers that holds only the negative numbers
+negative_numbers = [number for number in numbers if number < 0]
+negative_numbers
 
 # Exercise 14 - use a list comprehension w/ a conditional in order to produce a list of numbers with 2 or more numerals
+number_with_2_or_more_digits = [number for number in numbers if len(str(number)) >= 2 and (number < -10 or number > 0)]
+number_with_2_or_more_digits
 
 # Exercise 15 - Make a variable named numbers_squared that contains the numbers list with each element squared. Output is [4, 9, 16, etc...]
+numbers_squared = [number ** 2 for number in numbers]
+print(numbers)
+numbers_squared
 
 # Exercise 16 - Make a variable named odd_negative_numbers that contains only the numbers that are both odd and negative.
+odd_negative_numbers = [number for number in numbers if number % 2 != 0 and number < 0]
+odd_negative_numbers
 
 # Exercise 17 - Make a variable named numbers_plus_5. In it, return a list containing each number plus five. 
+numbers_plus_5 = [number + 5 for number in numbers]
+numbers_plus_5
 
-# BONUS Make a variable named "primes" that is a list containing the prime numbers in the numbers list. *Hint* you may want to make or find a helper function that determines if a given number is prime or not.
+# BONUS Make a variable named "primes" that is a list containing the prime numbers in the numbers list. 
+# *Hint* you may want to make or find a helper function that determines if a given number is prime or not.
